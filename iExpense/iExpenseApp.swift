32504f11246bc7ffd7da4a94e13967d85a14cbd3
Гамид Халидов <khalidov-gamid@mail.ru>
2024-05-08 22:19:47 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Gamıd Khalıdov on 08.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
